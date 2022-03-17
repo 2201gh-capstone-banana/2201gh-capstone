e67025f163b7c940e426542e60f780d7b0badd45
Anh Duong <anhhduong1609@gmail.com>
2022-03-17 17:54:15 -0400

    teachable machine models

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,26 +1,103 @@
-import React from 'react'
-import {connect} from 'react-redux'
-
+import React, { useRef } from "react";
+import { connect } from "react-redux";
+import * as tf from "@tensorflow/tfjs";
+import * as tmImage from "@teachablemachine/image";
+import Webcam from "react-webcam";
+//import { drawHand } from "./utilities";
 /**
  * COMPONENT
  */
-export const Home = props => {
-  const {username} = props
+
+export const Home = (props) => {
+  const webcamRef = useRef(null);
+  // const canvasRef = useRef(null);
+
+  const URL = "https://teachablemachine.withgoogle.com/models/HBBfwFtF-/";
+
+  let model, webcam, labelContainer, maxPredictions;
+
+  const checkpointURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
+  const loadModel = async () => {
+    const model = await tmImage.load(checkpointURL, metadataURL);
+    console.log("model-----", model);
+    setInterval(() => {
+      detect(model);
+    }, 3000);
+  };
+
+  //Loop and detect hands
+
+  async function detect(model) {
+    // predict can take in an image, video or canvas html element
+    // let prediction; what is this?
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      // canvasRef.current.width = videoWidth;
+      // canvasRef.current.height = videoHeight;
+
+      let prediction = await model.predict(webcamRef);
+      console.log("PREDICTION-----", prediction);
+      // for (let i = 0; i < maxPredictions; i++) {
+      //     const classPrediction =
+      //         prediction[i].className + ': ' + prediction[i].probability.toFixed(2);
+      //     // labelContainer.childNodes[i].innerHTML = classPrediction;
+      // }
+      //   drawHand();
+    }
+  }
+  loadModel();
+  // const { username } = props;
 
   return (
     <div>
-      <h3>Welcome, {username}</h3>
-    </div>
-  )
-}
+      <Webcam
+        ref={webcamRef}
+        style={{
+          marginTop: -240,
+          position: "absolute",
+          zIndex: 9,
+          width: 540,
+          height: 480,
+          backgroundColor: "black",
+        }}
+      />
 
+      {/* <canvas
+        ref={canvasRef}
+        style={{
+          position: "absolute",
+          marginTop: -240,
+          textAlign: "center",
+          zIndex: 9,
+          width: 540,
+          height: 480,
+          // background: "red",
+        }}
+      /> */}
+    </div>
+  );
+};
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
-    username: state.auth.username
-  }
-}
+    username: state.auth.username,
+  };
+};
 
-export default connect(mapState)(Home)
+export default connect(mapState)(Home);
