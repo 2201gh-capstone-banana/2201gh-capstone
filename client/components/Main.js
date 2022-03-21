a2e0e7117c9eb377a1f83322f8fc9279a5709363
Amber Lin <amberlinyq@gmail.com>
2022-03-21 18:33:38 -0400

    delete TM

--- client/components/Main.js
+++ client/components/Main.js
@@ -47,16 +47,9 @@ export const Home = props => {
 	const canvasRef = useRef(null)
 	const [translation, setTranslation] = useState(null)
 
-	const URL = 'https://teachablemachine.withgoogle.com/models/86Rqg3NFc/'
-	const checkpointURL = URL + 'model.json'
-	const metadataURL = URL + 'metadata.json'
-
 	const loadModel = async () => {
-		const model = await tmImage.load(checkpointURL, metadataURL)
 		const net = await handpose.load()
-
 		const netFace = await blazeface.load()
-		//pose
 		const detectorConfig = {
 			architecture: 'MobileNetV1',
 			outputStride: 16,
@@ -67,8 +60,6 @@ export const Home = props => {
 			poseDetection.SupportedModels.PoseNet,
 			detectorConfig
 		)
-
-		//both hands detection
 		const modelBothHands = handPoseDetection.SupportedModels.MediaPipeHands
 		const detectorConfigBothHands = {
 			runtime: 'tfjs',
@@ -83,25 +74,13 @@ export const Home = props => {
 
 		console.log('net', net.pipeline.maxHandsNumber)
 		setInterval(() => {
-			detect(model, net, netFace, netPose, netBothHands)
-			//this function is being called so quickly, you want to make it as optimized
-			//as possible
-			//"the fact you're using a setInterval could be a problem"
-			//it's asynchronous, so it's possible that two detects can happen at the same time.
-			//detect function sets a state on the component that says detection is happening.
-			//when detect function is completed you change that state back to false
-			//so you can create a gaurd that prevents another detect function
-			//request animation frame
-			//if resources are available, and I can perform this function.
-			//Window.requestAnimationFrame(detect())
-			//then within detect, call this again. sort of recursive for detect
-			//this means that the detect funtions are less likely to overlap with each other
-		}, 100)
+			detect(net, netFace, netPose, netBothHands)
+		}, 1000)
 	}
 
 	//Loop and detect hands
 
-	async function detect(model, net, netFace, netPose, netBothHands) {
+	async function detect(net, netFace, netPose, netBothHands) {
 		// predict can take in an image, video or canvas html element
 
 		if (
@@ -109,12 +88,6 @@ export const Home = props => {
 			webcamRef.current !== null &&
 			webcamRef.current.video.readyState === 4
 		) {
-			//we don't need to use this every time we call detect. maybe we should move this outside
-			//of the detect function context.
-			//go through this code and ensure that everything is actually something we have to redo
-			//in order to detect. if not, we can find a way to do it once in load model, keep
-			//a reference using useRef, and use reference to it.
-			// Get Video Properties
 			const video = webcamRef.current.video
 			const videoWidth = webcamRef.current.video.videoWidth
 			const videoHeight = webcamRef.current.video.videoHeight
@@ -141,44 +114,12 @@ export const Home = props => {
 			console.log('hand', hand)
 			console.log('both hands are', bothHands)
 			console.log('pose is', pose)
-
-			//this grabbing of the 2d context, this shouldn't need to be done more than once.
-			//you should be able to grab context and have reference to it, and use it in
-			//every detect call. we're doing this often enough to make it matter.
-			//want to dig through detect code and sus out whether everything we're doing
-			//in detect code absolutely needed
-			//draw mesh
 			const ctx = canvasRef.current.getContext('2d')
 
 			//drawHand(hand, ctx);
 			drawBothHands(bothHands, ctx)
 			drawFace(face, ctx)
 			drawPose(pose, ctx)
-			// if (bothHands.length === 2) {
-			//   console.log("get inside both hands???");
-			//   const gestureEstimatorForBothHand = new fp.GestureEstimator([
-			//     niceGesture,
-			//   ]);
-			//   const gestureLeftHand = await gestureEstimatorForBothHand.estimate(
-			//     bothHands[0].keypoints,
-			//     8
-			//   );
-			//   console.log("gesture left hand is", gestureLeftHand);
-			//   const gestureRightHand = await gestureEstimatorForBothHand.estimate(
-			//     bothHands[1].keypoints,
-			//     8
-			//   );
-			//   console.log("gesture right hand is", gestureRightHand);
-			//   if (gestureLeftHand.gestures && gestureRightHand.gestures) {
-			//     if (
-			//       gestureLeftHand.gestures[0].name === "nice" &&
-			//       gestureRightHand.gestures[0].name === "nice"
-			//     ) {
-			//       setTranslation("nice");
-			//     }
-			//   }
-			//   //make detections for hands and finger gestures
-			// }
 			if (bothHands.length === 1 && hand.length > 0 && face.length > 0) {
 				const gestureEstimator = new fp.GestureEstimator([
 					paperGesture,
