6290e34f2136dcfb7218ca6cdd7296376da54d26
Kimberley <kimberley@email.com>
2022-03-20 22:24:53 -0400

    feat(client/components): Added funcanility to signin page.

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+/* Components. */
 import Brand from './Brand'
 import AuthInput from './AuthInput'
+
+/* Thunks and action creators. */
+import { manualSignin, _clearAlert, signup } from '../store/auth'
 
 class AuthForm extends React.Component {
 	constructor() {
@@ -12,13 +16,18 @@ class AuthForm extends React.Component {
 			username: '',
 			password: '',
 			firstName: '',
-			lastName: ''
+			lastName: '',
+			email: ''
 		}
 		this.handleAuthType = this.handleAuthType.bind(this)
 		this.handleChange = this.handleChange.bind(this)
+		this.handleSubmit = this.handleSubmit.bind(this)
 	}
 
 	handleAuthType() {
+		/* If there is an alert clear it when the user toggles between signin and signup. */
+		this.props.clearAlert()
+
 		this.setState(prv => {
 			return { signin: !prv.signin }
 		})
@@ -28,15 +37,37 @@ class AuthForm extends React.Component {
 		this.setState({ [evt.target.name]: evt.target.value })
 	}
 
+	handleSubmit(evt) {
+		evt.preventDefault()
+
+		/* Checks if the user is on signin or signup. */
+		if (this.state.signin) {
+			const userData = {
+				username: this.state.username,
+				password: this.state.password
+			}
+			this.props.manualSignin(userData)
+		} else {
+			const userData = {
+				username: this.state.username,
+				password: this.state.password,
+				firstName: this.state.firstName,
+				lastName: this.state.lastName,
+				email: this.state.email
+			}
+
+			this.props.signup(userData)
+		}
+	}
+
 	render() {
-		console.log(this.state)
 		return (
 			<div className="auth__wrapper">
 				<header className="brand__wrapper">
 					<Brand />
 				</header>
 
-				<form className="authForm">
+				<form onSubmit={this.handleSubmit} className="authForm">
 					<AuthInput
 						for="username"
 						onChange={this.handleChange}
@@ -60,7 +91,17 @@ class AuthForm extends React.Component {
 								onChange={this.handleChange}
 								value={this.state.lastName}
 							/>
+
+							<AuthInput
+								for="email"
+								onChange={this.handleChange}
+								value={this.state.email}
+							/>
 						</>
+					) : null}
+
+					{this.props.alert ? (
+						<div className="auth__alert">{this.props.alert}</div>
 					) : null}
 
 					<button className="authSubmit" type="submit">
@@ -79,4 +120,18 @@ class AuthForm extends React.Component {
 	}
 }
 
-export default AuthForm
+const mapStateToProps = state => {
+	return {
+		alert: state.auth.alert
+	}
+}
+
+const mapDispatchToProps = dispatch => {
+	return {
+		manualSignin: userData => dispatch(manualSignin(userData)),
+		clearAlert: () => dispatch(_clearAlert()),
+		signup: userData => dispatch(signup(userData))
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AuthForm)
