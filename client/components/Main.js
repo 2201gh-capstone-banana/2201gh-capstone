a2d46f5b0e3427c3013813a666e45e53088f4e71
kroy94 <kelseyroy@mac.com>
2022-03-23 17:39:33 -0400

    addition

--- client/components/Main.js
+++ client/components/Main.js
@@ -50,104 +50,99 @@ export const Main = props => {
 	const [translation, setTranslation] = useState(null)
 	// const [isDetectRunning, setIsDetectRunning] = useState(false)
 	const netRef = useRef(null);
+	console.log("NET REF IS THIS:", netRef)
 	const handRef = useRef(null);
 
-	const loadModel = async () => {
-
-		const net = await handpose.load()	
-		netRef.current = net;
-		// setModel(net);
-		webcamInit();
-		// const requestanimationframe = window.requestAnimationFrame(loop)
-		// requestanimationframe(net);
-	}
-
-	const webcamInit = () => {
-		console.log("webcamInit is running...")
-		if (
-			webcamRef.current !== 'undefined' &&
-			webcamRef.current !== null &&
-			webcamRef.current.video.readyState === 4
-		) {
-			const video = webcamRef.current.video
-			const videoWidth = webcamRef.current.video.videoWidth
-			const videoHeight = webcamRef.current.video.videoHeight
-			// Set video width
-			webcamRef.current.video.width = videoWidth
-			webcamRef.current.video.height = videoHeight
-
-			//  Set canvas height and width
-			canvasRef.current.width = videoWidth
-			canvasRef.current.height = videoHeight
-
-			window.requestAnimationFrame(loop)
-
-		} else {
-			console.log("this is in the else part of webcam Init, it did not make it through if")
-		}
-	}
-
-	async function loop() {
-		console.log('inside loop')
-		await detect(netRef.current)
-		window.requestAnimationFrame(loop)
-		// const requestanimationframe = window.requestAnimationFrame(loop)
-		// requestanimationframe(net)
-	}
-
-	//Loop and detect hands
-
-	// async function detect(model, net, netFace, netPose, netBothHands) {
-	async function detect(net) {
-		// setIsDetectRunning(true);
-		// const net = await handpose.load()
-		console.log("detect function is running...")
-		const video = webcamRef.current.video
-		// predict can take in an image, video or canvas html element
-		//make detections for hand
-		const estimationConfig = { flipHorizontal: false }
-
-		const hand = await net.estimateHands(video)
-
-		const ctx = canvasRef.current.getContext('2d')
-
-		drawHand(hand, ctx);
-		//make detections for face
-		// const returnTensors = false
-
-		if (hand.length > 0) {
-			const gestureEstimator = new fp.GestureEstimator([
-				...letters.allLetters,
-				paperGesture,
-				loveYouGesture,
-				pleaseGesture,
-				youGesture
-			])
-
-			// 8 is the confidence level
-			const gesture = await gestureEstimator.estimate(hand[0].landmarks, 8)
-			if (gesture.gestures && gesture.gestures.length > 0) {
-				const score = gesture.gestures.map(prediction => prediction.score)
-
-				const maxScore = score.indexOf(Math.max.apply(null, score))
-				const gestureName = gesture.gestures[maxScore].name
-				console.log('gestures name is -', gesture.gestures[maxScore].name)
-
-				setTranslation(gestureName)
-			}
-
-		} else if (hand.length === 0) {
-			setTranslation(null)
-			return
-		}
-		// setIsDetectRunning(false);
-	}
-
 	useEffect(() => {
-		loadModel()
-		console.log('use effect for component did mount')
-		// webcamInit()
-		// console.log('after webcam init')
+		const loadModel = async () => {
+			console.log("PLEASE ONLY BE CALLED ONCE")
+			const net = await handpose.load()
+			netRef.current = net;
+			// setModel(net);
+			webcamInit();
+			// const requestanimationframe = window.requestAnimationFrame(loop)
+			// requestanimationframe(net);
+		}
+
+		const webcamInit = () => {
+			console.log("webcamInit is running...")
+			if (
+				webcamRef.current !== 'undefined' &&
+				webcamRef.current !== null &&
+				webcamRef.current.video.readyState === 4
+			) {
+				const video = webcamRef.current.video
+				const videoWidth = webcamRef.current.video.videoWidth
+				const videoHeight = webcamRef.current.video.videoHeight
+				// Set video width
+				webcamRef.current.video.width = videoWidth
+				webcamRef.current.video.height = videoHeight
+
+				//  Set canvas height and width
+				canvasRef.current.width = videoWidth
+				canvasRef.current.height = videoHeight
+
+				window.requestAnimationFrame(loop)
+
+			} else {
+				console.log("this is in the else part of webcam Init, it did not make it through if")
+			}
+		}
+
+		async function loop() {
+			console.log('inside loop')
+			await detect(netRef.current)
+			window.requestAnimationFrame(loop)
+			// const requestanimationframe = window.requestAnimationFrame(loop)
+			// requestanimationframe(net)
+		}
+
+		//Loop and detect hands
+
+		async function detect(net) {
+			// setIsDetectRunning(true);
+			console.log("detect function is running...")
+			const video = webcamRef.current.video
+			// predict can take in an image, video or canvas html element
+			//make detections for hand
+			const estimationConfig = { flipHorizontal: false }
+
+			const hand = await net.estimateHands(video)
+
+			const ctx = canvasRef.current.getContext('2d')
+
+			drawHand(hand, ctx);
+			//make detections for face
+			// const returnTensors = false
+
+			if (hand.length > 0) {
+				const gestureEstimator = new fp.GestureEstimator([
+					...letters.allLetters,
+					paperGesture,
+					loveYouGesture,
+					pleaseGesture,
+					youGesture
+				])
+
+				// 8 is the confidence level
+				const gesture = await gestureEstimator.estimate(hand[0].landmarks, 8)
+				if (gesture.gestures && gesture.gestures.length > 0) {
+					const score = gesture.gestures.map(prediction => prediction.score)
+
+					const maxScore = score.indexOf(Math.max.apply(null, score))
+					const gestureName = gesture.gestures[maxScore].name
+					console.log('gestures name is -', gesture.gestures[maxScore].name)
+
+					setTranslation(gestureName)
+				}
+
+			} else if (hand.length === 0) {
+				setTranslation(null)
+				return
+			}
+			// setIsDetectRunning(false);
+		}
+		loadModel();
 	}, [])
 
 	// useEffect(() => {
