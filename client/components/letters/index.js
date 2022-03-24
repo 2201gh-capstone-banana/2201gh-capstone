6c9d6bdc577664276165d01790fcf8d123e95c12
Anh Duong <anhhduong1609@gmail.com>
2022-03-24 02:38:24 -0400

    Learning Page

--- client/components/letters/index.js
+++ client/components/letters/index.js
@@ -1,29 +1,29 @@
-import letterA from "./letterA";
-import letterB from "./letterB";
-import letterC from "./letterC";
-import letterD from "./letterD";
-import letterE from "./letterE";
-import letterF from "./letterF";
-import letterG from "./letterG";
-import letterH from "./letterH";
-import letterI from "./letterI";
-import letterJ from './letterJ';
-import letterK from './letterK';
-import letterL from './letterL';
-import letterM from './letterM';
-import letterN from './letterN';
-import letterO from './letterO';
-import letterP from './letterP';
-import letterQ from './letterQ';
-import letterR from './letterR';
-import letterS from './letterS';
-import letterT from './letterT';
-import letterU from './letterU';
-import letterV from './letterV';
-import letterW from './letterW';
-import letterX from './letterX';
-import letterY from './letterY';
-import letterZ from './letterZ';
+import letterA from './letterA'
+import letterB from './letterB'
+import letterC from './letterC'
+import letterD from './letterD'
+import letterE from './letterE'
+import letterF from './letterF'
+import letterG from './letterG'
+import letterH from './letterH'
+import letterI from './letterI'
+import letterJ from './letterJ'
+import letterK from './letterK'
+import letterL from './letterL'
+import letterM from './letterM'
+import letterN from './letterN'
+import letterO from './letterO'
+import letterP from './letterP'
+import letterQ from './letterQ'
+import letterR from './letterR'
+import letterS from './letterS'
+import letterT from './letterT'
+import letterU from './letterU'
+import letterV from './letterV'
+import letterW from './letterW'
+import letterX from './letterX'
+import letterY from './letterY'
+import letterZ from './letterZ'
 
 export const letters = {
 	allLetters: [
@@ -54,4 +54,4 @@ export const letters = {
 		letterY,
 		letterZ
 	]
-};
+}
--- client/components/letters/letterA.js
+++ client/components/letters/letterA.js
@@ -1,32 +1,27 @@
-import {Finger, FingerCurl, FingerDirection, GestureDescription} from 'fingerpose';
+import { Finger, FingerCurl, FingerDirection, GestureDescription } from 'fingerpose'
 
 const letterA = new GestureDescription('A')
+export default letterA
 
 //Thumb
-letterA.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0);
-letterA.addDirection(Finger.Index, FingerDirection.DiagonalUpRight, 0.70);
-// aSign.addDirection(Finger.Index, FingerDirection.DiagonalUpLeft, 0.70);
+letterA.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0)
+letterA.addDirection(Finger.Index, FingerDirection.DiagonalUpRight, 0.7)
 // letterA.addDirection(Finger.Index, FingerDirection.DiagonalUpLeft, 0.70);
 
-
 //Index
-letterA.addCurl(Finger.Index, FingerCurl.FullCurl, 1);
-letterA.addDirection(Finger.Index, FingerDirection.VerticalUp, 0.70);
-// aSign.addDirection(Finger.Index, FingerDirection.DiagonalUpLeft, 0.70);
+letterA.addCurl(Finger.Index, FingerCurl.FullCurl, 1)
+letterA.addDirection(Finger.Index, FingerDirection.VerticalUp, 0.7)
 // letterA.addDirection(Finger.Index, FingerDirection.DiagonalUpLeft, 0.70);
 
 //Middle
-letterA.addCurl(Finger.Middle, FingerCurl.FullCurl, 1);
-letterA.addDirection(Finger.Middle, FingerDirection.VerticalUp, 0.70);
-// aSign.addDirection(Finger.Middle, FingerDirection.DiagonalUpLeft, 0.70);
+letterA.addCurl(Finger.Middle, FingerCurl.FullCurl, 1)
+letterA.addDirection(Finger.Middle, FingerDirection.VerticalUp, 0.7)
 // letterA.addDirection(Finger.Middle, FingerDirection.DiagonalUpLeft, 0.70);
 
 //Ring
-letterA.addCurl(Finger.Ring, FingerCurl.FullCurl, 1);
-letterA.addDirection(Finger.Ring, FingerDirection.VerticalUp, 0.70);
+letterA.addCurl(Finger.Ring, FingerCurl.FullCurl, 1)
+letterA.addDirection(Finger.Ring, FingerDirection.VerticalUp, 0.7)
 
 //Pinky
-letterA.addCurl(Finger.Pinky, FingerCurl.FullCurl, 1);
-letterA.addDirection(Finger.Pinky, FingerDirection.VerticalUp, 0.70);
-
-export default letterA;
+letterA.addCurl(Finger.Pinky, FingerCurl.FullCurl, 1)
+letterA.addDirection(Finger.Pinky, FingerDirection.VerticalUp, 0.7)
