8b891364c2e00681a9557012ad7acd8d1159d0d2
kroy94 <kelseyroy@mac.com>
2022-03-24 16:29:52 -0400

    adding backend functionality

--- server/db/index.js
+++ server/db/index.js
@@ -4,11 +4,14 @@ const db = require('./db')
 
 const User = require('./models/User')
 
+const Word = require('./models/Word')
+
 //associations could go here!
 
 module.exports = {
   db,
   models: {
     User,
+    Word
   },
 }
