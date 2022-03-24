b904a0dffba119f3e27b6ec85a07c85647426528
Anh Duong <anhhduong1609@gmail.com>
2022-03-24 18:37:25 -0400

    ready to merge to main

--- client/App.js
+++ client/App.js
@@ -1,14 +1,22 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import Routes from './Routes'
 import '../css/index.css'
+import { fetchAllLetters } from './store/letters'
 
 const App = () => {
+	const dispatch = useDispatch()
+	//componentDidMount equivalent
+	useEffect(() => {
+		dispatch(fetchAllLetters())
+	}, [])
+
+	// const allLetterInfo = useSelector(state => state.lettersReducer)
+	// console.log('all letter info', allLetterInfo)
 	return (
 		<div>
 			<Routes />
 		</div>
 	)
 }
-
 export default App
