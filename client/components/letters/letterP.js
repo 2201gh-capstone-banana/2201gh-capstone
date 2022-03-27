1cc507a18121e46d5526826bae90483dd15fc739
Anh Duong <anhhduong1609@gmail.com>
2022-03-27 13:31:15 -0400

    Updated all letters - letters still got confusion including N, S, T, Y

--- client/components/letters/letterP.js
+++ client/components/letters/letterP.js
@@ -31,27 +31,27 @@ const letterP = new GestureDescription('P')
 // 	letterP.addDirection(finger, FingerDirection.DiagonalUpRight, 1.0)
 // }
 
-letterP.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0);
-letterP.addDirection(Finger.Thumb, FingerDirection.HorizontalLeft, 1.0);
-letterP.addDirection(Finger.Thumb, FingerDirection.HorizontalRight, 1.0);
+letterP.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0)
+letterP.addDirection(Finger.Thumb, FingerDirection.HorizontalLeft, 1.0)
+letterP.addDirection(Finger.Thumb, FingerDirection.HorizontalRight, 1.0)
 
-letterP.addCurl(Finger.Index, FingerCurl.NoCurl, 1.0);
-letterP.addDirection(Finger.Index, FingerDirection.HorizontalLeft, 1.0);
-letterP.addDirection(Finger.Index, FingerDirection.HorizontalRight, 1.0);
+letterP.addCurl(Finger.Index, FingerCurl.NoCurl, 1.0)
+letterP.addDirection(Finger.Index, FingerDirection.HorizontalLeft, 1.0)
+letterP.addDirection(Finger.Index, FingerDirection.HorizontalRight, 1.0)
 
-letterP.addCurl(Finger.Middle, FingerCurl.NoCurl, 1.0);
-letterP.addCurl(Finger.Middle, FingerCurl.HorizontalLeft, 1);
-letterP.addCurl(Finger.Middle, FingerCurl.HorizontalRight, 1);
-letterP.addCurl(Finger.Middle, FingerCurl.DiagonalDownLeft, 1);
-letterP.addCurl(Finger.Middle, FingerCurl.DiagonalDownRight, 1);
+letterP.addCurl(Finger.Middle, FingerCurl.NoCurl, 1.0)
+letterP.addCurl(Finger.Middle, FingerCurl.HorizontalLeft, 1)
+letterP.addCurl(Finger.Middle, FingerCurl.HorizontalRight, 1)
+letterP.addCurl(Finger.Middle, FingerCurl.DiagonalDownLeft, 1)
+letterP.addCurl(Finger.Middle, FingerCurl.DiagonalDownRight, 1)
 
 for (let finger of [Finger.Ring, Finger.Pinky]) {
 	//   letterP.addCurl(finger, FingerCurl.NoCurl, 1.0);
-	letterP.addCurl(finger, FingerCurl.FullCurl, 1.0);
-	letterP.addCurl(finger, FingerCurl.HalfCurl, 0.9);
-	// letterP.addDirection(finger, FingerDirection.HorizontalRight, 1.0);
-	// letterP.addDirection(finger, FingerDirection.HorizontalLeft, 1.0);
-	letterP.addDirection(finger, FingerDirection.DiagonalDownRight, 1.0);
-	letterP.addDirection(finger, FingerDirection.DiagonalDownLeft, 1.0);
+	letterP.addCurl(finger, FingerCurl.FullCurl, 1.0)
+	letterP.addCurl(finger, FingerCurl.HalfCurl, 0.9)
+	letterP.addDirection(finger, FingerDirection.HorizontalRight, 1.0)
+	letterP.addDirection(finger, FingerDirection.HorizontalLeft, 1.0)
+	letterP.addDirection(finger, FingerDirection.DiagonalDownRight, 1.0)
+	letterP.addDirection(finger, FingerDirection.DiagonalDownLeft, 1.0)
 }
-export default letterP;
+export default letterP
--- client/components/letters/letterY.js
+++ client/components/letters/letterY.js
@@ -2,41 +2,40 @@ import { Finger, FingerCurl, FingerDirection, GestureDescription } from 'fingerp
 
 const letterY = new GestureDescription('Y')
 
-//Thumb
+// //Thumb
+// letterY.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0)
+// letterY.addDirection(Finger.Index, FingerDirection.DiagonalUpRight, 0.7)
+
+// //Index
+// letterY.addCurl(Finger.Index, FingerCurl.FullCurl, 1)
+// letterY.addDirection(Finger.Index, FingerDirection.VerticalUp, 0.7)
+
+// //Middle
+// letterY.addCurl(Finger.Middle, FingerCurl.FullCurl, 1)
+// letterY.addDirection(Finger.Middle, FingerDirection.VerticalUp, 0.7)
+
+// //Ring
+// letterY.addCurl(Finger.Ring, FingerCurl.FullCurl, 1)
+// letterY.addDirection(Finger.Ring, FingerDirection.VerticalUp, 0.7)
+
+// //Pinky
+// letterY.addCurl(Finger.Pinky, FingerCurl.NoCurl, 1)
+// letterY.addDirection(Finger.Pinky, FingerDirection.DiagonalUpLeft, 0.7)
+
 letterY.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0)
-letterY.addDirection(Finger.Index, FingerDirection.DiagonalUpRight, 0.7)
+letterY.addDirection(Finger.Thumb, FingerDirection.DiagonalUpRight, 1.0)
+letterY.addDirection(Finger.Thumb, FingerDirection.DiagonalUpLeft, 1.0)
 
-//Index
-letterY.addCurl(Finger.Index, FingerCurl.FullCurl, 1)
-letterY.addDirection(Finger.Index, FingerDirection.VerticalUp, 0.7)
-
-//Middle
-letterY.addCurl(Finger.Middle, FingerCurl.FullCurl, 1)
-letterY.addDirection(Finger.Middle, FingerDirection.VerticalUp, 0.7)
-
-//Ring
-letterY.addCurl(Finger.Ring, FingerCurl.FullCurl, 1)
-letterY.addDirection(Finger.Ring, FingerDirection.VerticalUp, 0.7)
-
-//Pinky
-letterY.addCurl(Finger.Pinky, FingerCurl.NoCurl, 1)
-letterY.addDirection(Finger.Pinky, FingerDirection.DiagonalUpLeft, 0.7)
-
-/*
-letterY.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
-letterY.addDirection(fp.Finger.Thumb, fp.FingerDirection.DiagonalUpRight, 1.0);
-letterY.addDirection(fp.Finger.Thumb, fp.FingerDirection.DiagonalUpLeft, 1.0);
-
-for (let finger of [fp.Finger.Ring, fp.Finger.Middle, fp.Finger.Index]) {
-  letterY.addCurl(finger, fp.FingerCurl.FullCurl, 1.0);
-  letterY.addCurl(finger, fp.FingerCurl.HalfCurl, 0.9);
-  letterY.addDirection(finger, fp.FingerDirection.VerticalUp, 1.0);
-  letterY.addDirection(finger, fp.FingerDirection.DiagonalUpLeft, 1.0);
-  letterY.addDirection(finger, fp.FingerDirection.DiagonalUpRight, 1.0);
+for (let finger of [Finger.Ring, Finger.Middle, Finger.Index]) {
+	letterY.addCurl(finger, FingerCurl.FullCurl, 1.0)
+	letterY.addCurl(finger, FingerCurl.HalfCurl, 0.9)
+	letterY.addDirection(finger, FingerDirection.VerticalUp, 1.0)
+	letterY.addDirection(finger, FingerDirection.DiagonalUpLeft, 1.0)
+	letterY.addDirection(finger, FingerDirection.DiagonalUpRight, 1.0)
 }
 
-letterY.addCurl(fp.Finger.Pinky, fp.FingerCurl.NoCurl, 1.0);
-letterY.addDirection(fp.Finger.Pinky, fp.FingerDirection.DiagonalUpRight, 1.0);
-letterY.addDirection(fp.Finger.Pinky, fp.FingerDirection.DiagonalUpLeft, 1.0);
-*/
+letterY.addCurl(Finger.Pinky, FingerCurl.NoCurl, 1.0)
+letterY.addDirection(Finger.Pinky, FingerDirection.DiagonalUpRight, 1.0)
+letterY.addDirection(Finger.Pinky, FingerDirection.DiagonalUpLeft, 1.0)
+
 export default letterY
