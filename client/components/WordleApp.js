fbba0f4f120ea25efb904c3da7719b595764f655
Amber Lin <amberlinyq@gmail.com>
2022-03-27 14:38:31 -0400

    Add sidebar and leaderboard

--- client/components/WordleApp.js
+++ client/components/WordleApp.js
@@ -1,34 +1,40 @@
-import React, { Component, useState, createContext } from 'react';
-import WordleBoard from './Wordle/WordleBoard';
-import WordleDetection from './WordleDetection';
-import { boardDefault, rows } from './Wordle/wordleUtilities';
+import React, { Component, useState, createContext } from 'react'
+import WordleBoard from './Wordle/WordleBoard'
+import WordleDetection from './WordleDetection'
+import { boardDefault, rows } from './Wordle/wordleUtilities'
+import Sidebar from './SideBar'
 
 // class WordleApp extends Component {
 
 //     render() {
-export const WordleAppContext = createContext();
+export const WordleAppContext = createContext()
 function WordleApp() {
-    const [board, setBoard] = useState(rows);
-    const [translation, setTranslation] = useState(null)
+	const [board, setBoard] = useState(rows)
+	const [translation, setTranslation] = useState(null)
 
-    return (
-        <div className='wordle-app'>
-            <h1>Wordle</h1>
-            <WordleAppContext.Provider value={{ board, setBoard, translation, setTranslation }}>
-                <div className='game'>
-                    <div id='webcam-parent'>
-                        <h2>{translation || 'Hold up a letter in ASL to begin the detection!'}</h2>
-                        {/* <h2>Detecting: {translation}</h2> */}
-                        <WordleDetection />
-                    </div>
-                    <div id='board-parent'>
-                        <WordleBoard />
-                    </div>
-                </div>
-            </WordleAppContext.Provider>
-        </div>
-    )
-    //     }
+	return (
+		<div className="wordle-app">
+			<h1>Wordle</h1>
+			<Sidebar />
+			<WordleAppContext.Provider
+				value={{ board, setBoard, translation, setTranslation }}>
+				<div className="game">
+					<div id="webcam-parent">
+						<h2>
+							{translation ||
+								'Hold up a letter in ASL to begin the detection!'}
+						</h2>
+						{/* <h2>Detecting: {translation}</h2> */}
+						<WordleDetection />
+					</div>
+					<div id="board-parent">
+						<WordleBoard />
+					</div>
+				</div>
+			</WordleAppContext.Provider>
+		</div>
+	)
+	//     }
 }
 
-export default WordleApp;
+export default WordleApp
