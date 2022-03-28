2f686123fc231d75b5d19c6dfb57d8969005427d
Kimberley <kimberley@email.com>
2022-03-28 14:48:14 -0400

    feat(client/components): Added text to about page.

--- client/components/About.js
+++ client/components/About.js
@@ -6,9 +6,24 @@ const About = () => {
 	return (
 		<div className="about__wrapper">
 			<div className="about">
-				<AboutRow />
-				<AboutRow />
-				<AboutRow />
+				<AboutRow>
+					<blockquote cite="https://www.nidcd.nih.gov/health/american-sign-language">
+						American Sign Language (ASL) is a complete, natural language that
+						has the same linguistic properties as spoken languages, with
+						grammar that differs from English. ASL is expressed by movements
+						of the hands and face. It is the primary language of many North
+						Americans who are deaf and hard of hearing and is used by some
+						hearing people as well.
+					</blockquote>
+					<p>
+						National Institute on Deafness and Other Communication Disorders
+					</p>
+				</AboutRow>
+				<AboutRow>
+					Bee My Voice uses machine learning to detect hand movements. It was
+					built as a Capstone project for the Grace Hopper Program at Fullstack
+					Academy.
+				</AboutRow>
 			</div>
 		</div>
 	)
--- client/components/AboutRow.js
+++ client/components/AboutRow.js
@@ -1,25 +1,12 @@
 import React from 'react'
 
-const AboutRow = () => {
+const AboutRow = props => {
 	return (
 		<div className="about__row">
 			<div className="hexagon about__row-left">
 				<img src="https://placehold.jp/350x350.png" alt="" />
 			</div>
-			<div className="about__row-right">
-				<p>
-					Gingerbread macaroon jelly beans sweet croissant lemon drops
-					gingerbread carrot cake toffee. Toffee sweet roll gummi bears
-					fruitcake jelly ice cream candy canes. Caramels danish chupa chups
-					apple pie shortbread carrot cake toffee jelly-o pastry.
-				</p>
-
-				<p>
-					Icing bonbon gingerbread sugar plum cookie pudding. Cupcake dessert
-					oat cake jelly-o caramels. Apple pie candy canes pastry jujubes
-					tootsie roll ice cream.
-				</p>
-			</div>
+			<div className="about__row-right">{props.children}</div>
 		</div>
 	)
 }
